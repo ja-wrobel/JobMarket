6e6ee0aa297e6a40950105ef5a615cfc525ef3cd
ja-wrobel <jakub.wrobel2563@gmail.com>
2024-07-11 13:50:43 +0200

    clear button for search input

--- client/src/components/inputs/SearchInput.jsx
+++ client/src/components/inputs/SearchInput.jsx
@@ -25,17 +25,36 @@ export default function SearchInput(){
             }
         }
     }
+    function clear(){
+        const input = document.getElementById('search-input');
+        input.value = '';
+        search();
+    }
     return(
         <>
-        <input spellCheck="false" type="search" id="search-input" style={
+        <input spellCheck="false" type="text" id="search-input" style={
             {   
-                backgroundColor: '#19197094',
+                backgroundColor: 'rgba(25, 25, 112, 0.58)',
                 borderRadius: '5px',
                 padding: '3px',
                 border: '2px groove rgb(35, 112, 255, 0.8)',
-                color: 'rgb(20, 190, 240)'
+                color: 'rgb(20, 190, 240)',
+                marginRight: '3px'
             }
-            } placeholder='What you seek?.. "strict"' onChange={search}></input>
+        } placeholder='What you seek?.. "strict"' onChange={search}></input>
+        <input type="button" value={'X'} style={
+            {
+                border: '2px groove rgb(35, 112, 255, 0.8)',
+                borderRadius: '5px',
+                padding: '3px 5px',
+                backgroundColor: 'rgba(25, 25, 112, 0.58)',
+                color: 'rgb(20, 190, 240)',
+                cursor: 'pointer'
+            }
+        }onMouseEnter={(e)=>{e.target.style.border = '2px groove rgb(80, 186, 243, 0.8)';e.target.style.backgroundColor = 'rgb(34 81 173 / 58%)';}}
+        onMouseLeave={(e)=>{e.target.style.border = '2px groove rgb(35, 112, 255, 0.8)';e.target.style.backgroundColor = 'rgba(25, 25, 112, 0.58)';}}
+        onClick={clear}
+        />
         </>
     );
 }
